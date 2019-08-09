9968e347c846675746dd01bbfed3438c21539932
SEKHAR JHA <sekharjha14@gmail.com>
2019-08-09 17:47:34 +0530

    initial commit

---
+++ src/main/java/com/example/blueoptima/BlueoptimaApplication.java
@@ -0,0 +1,13 @@
+package com.example.blueoptima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueoptimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueoptimaApplication.class, args);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/HomePage.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Log Analyzer</title>
+</head>
+<body>
+Log Analyzer
+</body>
+</html>
