c465eca3ceef9e37b5622160b9a545c5869a0724
SEKHAR JHA <sekharjha14@gmail.com>
2019-08-10 14:15:03 +0530

    file manipulation

--- src/main/java/com/example/blueoptima/Controller/HomeController.java
+++ src/main/java/com/example/blueoptima/Controller/HomeController.java
@@ -1,21 +1,54 @@
 package com.example.blueoptima.Controller;
 
 
+import com.example.blueoptima.CsvReader.Csv;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.gridfs.GridFsOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
 
 @Controller
 public class HomeController {
 
+
+    @Autowired
+    private GridFsOperations gridFsOperations;
+
+
     @GetMapping("/")
     public String HomePage(){
+        try {
+            String filePath="C:\\Users\\sekha\\Documents\\Data.csv";
+            Csv.readAllDataAtOnce(filePath);
+        }
+        catch (Exception e){
+
+        }
         return "HomePage";
     }
 
+
     @PostMapping("/output")
-    public String showResult(String msg, ModelMap map){
-        map.put("msg", msg);
+    public String showResult(MultipartFile files[],ModelMap map){
+        StringBuilder fileNames = new StringBuilder();
+
+        for (MultipartFile file : files) {
+            fileNames.append(file.getOriginalFilename() + " ");
+            try {
+                InputStream inputStream = file.getInputStream();
+
+                String fileId = gridFsOperations.store(inputStream, UUID.randomUUID().toString() + ".txt", "image/png").toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         return "result";
     }
 }
--- src/main/webapp/WEB-INF/jsp/HomePage.jsp
+++ src/main/webapp/WEB-INF/jsp/HomePage.jsp
@@ -1,15 +1,40 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>Log Analyzer</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
-Log Analyzer
-<form action="/output" method="post">
-    Type Input: <input id="msg" type="text" name="msg"><br><br>
-    <input type="submit" value="Submit">
+<style>
+    .button {
+        background-color: white;
+        border: ;
+        color: black;
+        padding: 10px 20px;
+        text-align: center;
+        font-size: 16px;
+        font-family: Comic sans MS;
+        cursor: pointer;
+    }
 
+    .button:hover {
+        background-color: lightgrey;
+    }
+
+    .button:active {
+        background-color: grey;
+    }
+
+</style>
+
+<title>Log Analyzer</title>
+</head>
+
+<body style="background-color:black;color:white;text-align:center;padding:200px 200px;">
+
+
+<h1><i>LOG ANALYZER</i></h1>
+<form action="/output" method="post" enctype="multipart/form-data">
+<input class="btn btn1" type="file" name="files">Upload File Here</input>
+    <button class="btn btn2" type="submit">Submit</button>
 </form>
 </body>
 </html>
